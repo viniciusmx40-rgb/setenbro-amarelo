d0edce28e136078554cfd52b909bb92dec6d1b05
vinicius_KJ <viniciusmx40@gmail.com>
2025-09-09 13:50:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const mensagens = [
-"Você é mais forte do que imagina(❤´艸｀❤)"
-"Nunca se esqueça: sua vida importa(❤ ω ❤)",
-"Você não está sozinho, estamos juntos(´▽`ʃ♡ƪ)",
-"Acredite: dias melhores virão(✿◡‿◡)";
-"Sempre existe esperança 💖"
+  "Você é mais forte do que imagina (❤´艸｀❤)",
+  "Nunca se esqueça: sua vida importa (❤ ω ❤)",
+  "Você não está sozinho, estamos juntos (´▽`ʃ♡ƪ)",
+  "Acredite: dias melhores virão (✿◡‿◡)",
+  "Sempre existe esperança 💖"
 ];
+
 document.getElementById("btnMensagem").addEventListener("click", () => {
-const aleatoria = mensagens [Math.floor(Math.random() * mensagens.length)]:
-document.getElementById("mensagem").innerText = aleatoria;
+  const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+  document.getElementById("mensagem").innerText = aleatoria;
 });
