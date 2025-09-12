7d550a6a98c9f5df65ceae16e45b2901dcbbae7d
vinicius_KJ <viniciusmx40@gmail.com>
2025-09-12 10:16:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mensagens = [
   "Você é mais forte do que imagina (❤´艸｀❤)",
   "Nunca se esqueça: sua vida importa (❤ ω ❤)",
   "Você não está sozinho, estamos juntos (´▽`ʃ♡ƪ)",
-  "Acredite: dias melhores virão (✿◡‿◡)",
+  "Acredite: dias melhores virão (✿◡‿◡)#SetembroAmarelo | VÍDEO MOTIVACIONAL DISNEY - NÃO ...",
   "Sempre existe esperança 💖"
 ];
 
